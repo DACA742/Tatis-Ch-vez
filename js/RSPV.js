84d815851743dc9d0970ea340d90754942a61a1f
DACA742 <danielalejandrochavezarteaga@gmail.com>
2024-11-04 21:23:51 -0600

    1

--- js/RSPV.js
+++ js/RSPV.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
@@ -6,6 +7,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        } else {
             $.post('https://script.google.com/macros/s/AKfycbwaqHslEwT9HZpxGN8Y1zqhxXIvp39CL3os8WD2uUW9EbZYGX1EiSP5cHkobPJlHQgq9A/exec', data)
                 .done(function (data) {
                     console.log(data);
@@ -20,7 +25,7 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
-        
+        }
     });
 
 });
